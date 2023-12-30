358c13f2f80e21f2679da8974eae5df77539bf3a
mancatha <benmanks2015@gmail.com>
2023-12-30 23:31:11 +0000

    app

--- app.py
+++ app.py
@@ -13,20 +13,19 @@ categoricals = ['TENURE']
 
 # Define helper functions
 # Function to load the pipeline
-def load_pipeline(file_path = r"model\pipeline.pkl"):
+def load_pipeline(file_path="model/dt.joblib"):
     with open(file_path, "rb") as file:
         pipeline = joblib.load(file)
     return pipeline
 
+
 # Import the model
-model= XGBClassifier()
-model=joblib.load(r"model/xgb.joblib")
-model_1 = DecisionTreeClassifier()
-model_1 = joblib.loaf(r"model/dt.joblib")
+# Import the model using the load_pipeline function
+model = load_pipeline("model/dt.joblib")
 
 # Instantiate the pipeline
-Xgb_pipeline= load_pipeline()
-decision_tree_pipeline = load_pipeline()
+dt_pipeline= load_pipeline()
+#decision_tree_pipeline = load_pipeline()
 
 scaler = None
 encoder = None
@@ -34,7 +33,7 @@ encoder = None
 # Function to process inputs and return prediction
 
 
-def predict_customer_attrition(*args, pipeline=decision_tree_pipeline):
+def predict_customer_attrition(*args, pipeline=dt_pipeline):
     # Convert inputs into a dataframe
     input_data = pd.DataFrame([args], columns=expected_inputs)
     
@@ -51,32 +50,26 @@ def predict_customer_attrition(*args, pipeline=decision_tree_pipeline):
     # Return the prediction
     return {"Prediction: Customer is likely to Churn": prediction,
             "Prediction: Customer is likely not to Churn": 1 - prediction}
-# Define a function for text prediction
-def predict(text):
-    # Your text prediction code here
-    return "Prediction: " + text
-
+    
+  
       # Set up interface
     # Inputs
-TENURE= gr.Dropdown(label = "What is the duration of your network?", choices = ['I 18-21 month' 'K > 24 month' 'G 12-15 month' 'J 21-24 month'
- 'H 15-18 month' 'F 9-12 month' 'E 6-9 month' 'D 3-6 month'], value= 'I 18-21')
-MONTANT= gr.Slider(label= "What is your top-amount?", minimum= 15, maximum= 800, value= 10, interactive= True)    
+
+TENURE= gr.Dropdown(label = "What is the duration of your network?", choices = ['I 18-21 month' ,'K > 24 month', 'G 12-15 month' ,'J 21-24 month','H 15-18 month', 'F 9-12 month' ,'E 6-9 month' ,'D 3-6 month'],value ='I 18-21 month')
+MONTANT= gr.Slider(label= "What is your top-amount?", minimum= 15, maximum= 800, value= 10, interactive= True)
 FREQUENCE_RECH= gr.Slider(label= "What is number of times does you refilled your bundle?", minimum= 5, maximum= 200, value= 10, interactive= True)
-REVENUE = gr.Slider(label="What is your monthly income", mininum = 100,maximum=10000, value=200,interactive=True)
-ARPU_SEGMENT = gr.Slider(label ="What is your income over 90 days / 3",mininum = 1000,maximum=500000,value=1000,interactive=True)
-FREQUENCE = gr.Slider(label= "How do you how often do you use the service ",minimum = 10,maximum=200,value= 5,interactive=True)
-DATA_VOLUME = gr.Slider(label= "How many times do you have connections", mininmum =20,maximum = 1000,value= 2,interactive=True)
+REVENUE = gr.Slider(label="What is your monthly income", minimum = 100,maximum=10000, value=200,interactive=True)
+ARPU_SEGMENT = gr.Slider(label ="What is your income over 90 days / 3",minimum = 1000,maximum=500000,value=1000,interactive=True)
+FREQUENCE= gr.Slider(label= "How do you how often do you use the service ",minimum = 10,maximum=200,value= 5,interactive=True)
+DATA_VOLUME = gr.Slider(label= "How many times do you have connections", minimum =20,maximum = 1000,value= 2,interactive=True)
 ON_NET = gr.Slider(label = "How many time do you do inter expresso call", minimum = 5,maximum= 1000,value=3,interactive=True)
 ORANGE = gr.Slider(label = "How many time do you use orange to make calls(tigo)",minimum=5,maximum =100,value=2,interactive=True)
 TIGO= gr.Slider(label= "How many time do you use tigo networks", minimum=6,maximum=100,value =5,interactive=True)
 REGULARITY= gr.Slider(label= "How many number of times the are you active for 90 days",minimum=5,maximum=100,value=2,interactive=True)
-FREQ_TOP_PACK= gr.Slider(label="How many number of times does you been activated to the top pack packages",minimum=10,maximum=1000,value=5,intercative=True)
-
-
-
+FREQ_TOP_PACK= gr.Slider(label="How many number of times does you been activated to the top pack packages",minimum=10,maximum=1000,value=5,interactive=True)
 
 # Create the Gradio interface
-gr.Interface(inputs=['TENURE', 'MONTANT', 'FREQUENCE_RECH', 'REVENUE', ' ARPU_SEGMENT','FREQUENCE', 'DATA_VOLUME', 'ON_NET', 'ORANGE',' TIGO', ' REGULARITY', ' FREQ_TOP_PACK'],
+gr.Interface(inputs=[TENURE,MONTANT,FREQUENCE_RECH,REVENUE,ARPU_SEGMENT,FREQUENCE,DATA_VOLUME,ON_NET,ORANGE,TIGO,REGULARITY,FREQ_TOP_PACK],
              fn=predict_customer_attrition,
              outputs= gr.Label("Awaiting Submission...."),
              title="Telecommunication Customer Attrition Prediction App",
